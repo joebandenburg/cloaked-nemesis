cf6d01e97cfee2e302e24696572fba49c8e6eb13
Joe Bandenburg <joe@bandenburg.com>
2014-10-31 12:03:03 +0000

    Add full set of jshint options

--- src/app.js
+++ src/app.js
@@ -1,141 +1,140 @@
 // Everything is in seconds. You noob.
+(function() {
+    'use strict';
 
-/*global angular:false*/
-/*global requestAnimationFrame:false*/
-/*global performance:false*/
+    var app = angular.module('clockedNemesisApp', ['clockedNemesisDirectives']);
 
-var app = angular.module('clockedNemesisApp', ['clockedNemesisDirectives']);
-
-app.factory('requestAnimationFrameLoop', function ($rootScope) {
-    return function (callback) {
-        function loopCallback(time) {
-            $rootScope.$apply(function () {
-                callback(time);
-            });
-            requestAnimationFrame(loopCallback);
-        }
-        requestAnimationFrame(loopCallback);
-    };
-});
-
-app.controller('Main', function ($scope, requestAnimationFrameLoop) {
-    $scope.playerShip = {
-        shields: {
-            max: 100,
-            value: 100,
-            regenRate: 30,
-            powerUsage: 1000
-        },
-        power: {
-            max: 10000,
-            value: 10000,
-            boost: 1000,
-            regenRate: 0
-        },
-        hull: {
-            max: 100,
-            value: 100,
-            regenRate: 1,
-            boost: 1
-        }
-    };
-
-    $scope.enemyShip = {
-        shields: {
-            max: 100,
-            value: 100,
-            regenRate: 0,
-            powerUsage: 1000
-        },
-        power: {
-            max: 10000,
-            value: 10000,
-            boost: 0,
-            regenRate: 0
-        },
-        hull: {
-            max: 100,
-            value: 100,
-            regenRate: 1,
-            boost: 1
-        },
-        weapons: [{
-            fireRate: 0.5,
-            lastFired: 0,
-            damage: 20,
-            hitProbability: 0.5,
-            value: 0
-        }]
-    };
-
-    var lastTime = performance.now() / 1000;
-
-    function systemHit(system, damage)
-    {
-        if (system.value >= damage)
-        {
-            system.value -= damage;
-            damage = 0;
-        }
-        else
-        {
-            damage -= system.value;
-            system.value = 0;
-        }
-        return damage;
-    }
-
-    function systemDelta(system, delta)
-    {
-        system.value = Math.min(system.max, Math.max(0, system.value + delta));
-    }
-
-    function updateLoop(time) {
-        time /= 1000;
-        var timeDelta = time - lastTime;
-        lastTime = time;
-
-        // Use power
-        systemDelta($scope.playerShip.power,
-                    ($scope.playerShip.power.regenRate - $scope.playerShip.shields.powerUsage)* timeDelta);
-
-        if ($scope.playerShip.power.value > 0) {
-            // Regen shields
-            if ($scope.playerShip.shields.value < $scope.playerShip.shields.max) {
-                systemDelta($scope.playerShip.shields, $scope.playerShip.shields.regenRate * timeDelta);
+    app.factory('requestAnimationFrameLoop', function ($rootScope) {
+        return function (callback) {
+            function loopCallback(time) {
+                $rootScope.$apply(function () {
+                    callback(time);
+                });
+                requestAnimationFrame(loopCallback);
             }
+            requestAnimationFrame(loopCallback);
+        };
+    });
+
+    app.controller('Main', function ($scope, requestAnimationFrameLoop) {
+        $scope.playerShip = {
+            shields: {
+                max: 100,
+                value: 100,
+                regenRate: 30,
+                powerUsage: 1000
+            },
+            power: {
+                max: 10000,
+                value: 10000,
+                boost: 1000,
+                regenRate: 0
+            },
+            hull: {
+                max: 100,
+                value: 100,
+                regenRate: 1,
+                boost: 1
+            }
+        };
+
+        $scope.enemyShip = {
+            shields: {
+                max: 100,
+                value: 100,
+                regenRate: 0,
+                powerUsage: 1000
+            },
+            power: {
+                max: 10000,
+                value: 10000,
+                boost: 0,
+                regenRate: 0
+            },
+            hull: {
+                max: 100,
+                value: 100,
+                regenRate: 1,
+                boost: 1
+            },
+            weapons: [{
+                fireRate: 0.5,
+                lastFired: 0,
+                damage: 20,
+                hitProbability: 0.5,
+                value: 0
+            }]
+        };
+
+        var lastTime = window.performance.now() / 1000;
+
+        function systemHit(system, damage)
+        {
+            if (system.value >= damage)
+            {
+                system.value -= damage;
+                damage = 0;
+            }
+            else
+            {
+                damage -= system.value;
+                system.value = 0;
+            }
+            return damage;
         }
 
-        systemDelta($scope.playerShip.hull, $scope.playerShip.hull.regenRate * timeDelta);
+        function systemDelta(system, delta)
+        {
+            system.value = Math.min(system.max, Math.max(0, system.value + delta));
+        }
 
-        // Enemy fires
-        $scope.enemyShip.weapons.forEach(function (weapon) {
-            if (weapon.lastFired + weapon.fireRate < time) {
-                weapon.lastFired = time;
-                var wasHit = Math.random() < weapon.hitProbability;
-                if (wasHit) {
-                    var damage = weapon.damage;
-                    damage = systemHit($scope.playerShip.shields, damage);
-                    damage = systemHit($scope.playerShip.hull, damage);
+        function updateLoop(time) {
+            time /= 1000;
+            var timeDelta = time - lastTime;
+            lastTime = time;
+
+            // Use power
+            systemDelta($scope.playerShip.power,
+                        ($scope.playerShip.power.regenRate - $scope.playerShip.shields.powerUsage)* timeDelta);
+
+            if ($scope.playerShip.power.value > 0) {
+                // Regen shields
+                if ($scope.playerShip.shields.value < $scope.playerShip.shields.max) {
+                    systemDelta($scope.playerShip.shields, $scope.playerShip.shields.regenRate * timeDelta);
                 }
             }
-        });
-    }
 
-    requestAnimationFrameLoop(updateLoop);
+            systemDelta($scope.playerShip.hull, $scope.playerShip.hull.regenRate * timeDelta);
 
-    $scope.rechargeHull = function() {
-        systemDelta($scope.playerShip.hull, $scope.playerShip.hull.boost);
-    };
-
-    $scope.rechargePower = function($event) {
-        if ($event.button === 0)
-        {
-            systemDelta($scope.playerShip.power, $scope.playerShip.power.boost);
+            // Enemy fires
+            $scope.enemyShip.weapons.forEach(function (weapon) {
+                if (weapon.lastFired + weapon.fireRate < time) {
+                    weapon.lastFired = time;
+                    var wasHit = Math.random() < weapon.hitProbability;
+                    if (wasHit) {
+                        var damage = weapon.damage;
+                        damage = systemHit($scope.playerShip.shields, damage);
+                        damage = systemHit($scope.playerShip.hull, damage);
+                    }
+                }
+            });
         }
-        else
-        {
-            $scope.playerShip.power.regenRate += 5;
-        }
-    };
-});
+
+        requestAnimationFrameLoop(updateLoop);
+
+        $scope.rechargeHull = function() {
+            systemDelta($scope.playerShip.hull, $scope.playerShip.hull.boost);
+        };
+
+        $scope.rechargePower = function($event) {
+            if ($event.button === 0)
+            {
+                systemDelta($scope.playerShip.power, $scope.playerShip.power.boost);
+            }
+            else
+            {
+                $scope.playerShip.power.regenRate += 5;
+            }
+        };
+    });
+})();
--- src/directives.js
+++ src/directives.js
@@ -1,56 +1,60 @@
-var app = angular.module('clockedNemesisDirectives', []);
+(function() {
+    'use strict';
 
-app.directive('dial', function () {
-    function link(scope, element, attrs) {
-        var canvas = element.find('canvas')[0];
-        var ctx = canvas.getContext('2d');
-        var radius = 100;
-        var width = 2;
-        var progressColour = '#23a9b8';
-        var backgroundColour = '#333';
-        var imageSize = 320;
-        var minValue, maxValue;
+    var app = angular.module('clockedNemesisDirectives', []);
 
-        scope.$watch('colour', function (value) {
-            progressColour = value;
-        });
+    app.directive('dial', function () {
+        function link(scope, element) {
+            var canvas = element.find('canvas')[0];
+            var ctx = canvas.getContext('2d');
+            var radius = 100;
+            var width = 2;
+            var progressColour = '#23a9b8';
+            var backgroundColour = '#333';
+            var imageSize = 320;
+            var minValue, maxValue;
 
-        scope.$watch('minValue', function (value) {
-            minValue = value;
-        });
+            scope.$watch('colour', function (value) {
+                progressColour = value;
+            });
 
-        scope.$watch('maxValue', function (value) {
-            maxValue = value;
-        });
+            scope.$watch('minValue', function (value) {
+                minValue = value;
+            });
 
-        scope.$watch('value', function (value) {
-            if (minValue !== undefined && maxValue !== undefined) {
-                value = (value - minValue) / (maxValue - minValue) * 100;
-            }
-            scope.percentValue = value;
-            var center = imageSize / 2;
-            ctx.clearRect(0, 0, imageSize, imageSize);
-            ctx.strokeStyle = backgroundColour;
-            ctx.lineWidth = width;
-            ctx.beginPath();
-            ctx.arc(center, center, radius, -Math.PI / 2, Math.PI * 1.5, false);
-            ctx.stroke();
-            ctx.beginPath();
-            ctx.strokeStyle = progressColour;
-            ctx.arc(center, center, radius, -Math.PI / 2, -Math.PI / 2 + Math.PI * value / 100 * 2, false);
-            ctx.stroke();
-        });
-    }
-    return {
-        restrict: 'E',
-        scope: {
-            title: '@',
-            value: '=',
-            minValue: '=min',
-            maxValue: '=max',
-            colour: '@'
-        },
-        link: link,
-        template: '<div class="dial"><canvas width="320" height="320"></canvas><span class="value">{{percentValue | number:0}}%</span><span class="title">{{title}}</span></div>'
-    };
-});
+            scope.$watch('maxValue', function (value) {
+                maxValue = value;
+            });
+
+            scope.$watch('value', function (value) {
+                if (minValue !== undefined && maxValue !== undefined) {
+                    value = (value - minValue) / (maxValue - minValue) * 100;
+                }
+                scope.percentValue = value;
+                var center = imageSize / 2;
+                ctx.clearRect(0, 0, imageSize, imageSize);
+                ctx.strokeStyle = backgroundColour;
+                ctx.lineWidth = width;
+                ctx.beginPath();
+                ctx.arc(center, center, radius, -Math.PI / 2, Math.PI * 1.5, false);
+                ctx.stroke();
+                ctx.beginPath();
+                ctx.strokeStyle = progressColour;
+                ctx.arc(center, center, radius, -Math.PI / 2, -Math.PI / 2 + Math.PI * value / 100 * 2, false);
+                ctx.stroke();
+            });
+        }
+        return {
+            restrict: 'E',
+            scope: {
+                title: '@',
+                value: '=',
+                minValue: '=min',
+                maxValue: '=max',
+                colour: '@'
+            },
+            link: link,
+            template: '<div class="dial"><canvas width="320" height="320"></canvas><span class="value">{{percentValue | number:0}}%</span><span class="title">{{title}}</span></div>'
+        };
+    });
+})();
