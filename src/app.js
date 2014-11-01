464d85d4c18bd1dc305ddc1ed7e31ec89818a597
David Fooks <davidfooks@turbulenz.biz>
2014-11-01 16:55:03 +0000

    Code review changes

--- src/app.js
+++ src/app.js
@@ -45,26 +45,6 @@
             }
         };
 
-        function calculateShipStats()
-        {
-            _.each($scope.playerShipBaseStats, function (baseSystemStats, systemName) {
-                _.each($scope.baseSystemStats, function (statValue, statName) {
-                    $scope.playerShip[systemName][statName] = _.cloneDeep(statValue);
-                });
-            });
-
-            _.each($scope.playerShip.upgrades, function (level, upgradeName) {
-                var upgrade = upgrades[upgradeName];
-                _.each(upgrade.modifier, function (modifierFn, modifierName)
-                {
-                    for (var i = 1; i <= level; i += 1)
-                    {
-                        $scope.playerShip[upgrade.system][modifierName] += modifierFn(level);
-                    }
-                });
-            });
-        }
-
         $scope.playerShip = {
             upgrades: { }
         };
@@ -73,22 +53,32 @@
             $scope.playerShip.upgrades[key] = 0;
         });
 
-        _.each($scope.playerShipBaseStats, function (value, key) {
-            $scope.playerShip[key] = _.clone(value);
-            if (value.max)
-            {
-                $scope.playerShip[key].value = value.max;
-            }
-            else
-            {
-                $scope.playerShip[key].value = 0;
-            }
+        _.each($scope.playerShipBaseStats, function (baseSystemStats, systemName) {
+            $scope.playerShip[systemName] = {
+                value: baseSystemStats.max || 0
+            };
         });
+
+        function calculateShipStats() {
+            _.each($scope.playerShipBaseStats, function (baseSystemStats, systemName) {
+                _.each(baseSystemStats, function (statValue, statName) {
+                    $scope.playerShip[systemName][statName] = _.cloneDeep(statValue);
+                });
+            });
+
+            _.each($scope.playerShip.upgrades, function (level, upgradeName) {
+                var upgrade = upgrades[upgradeName];
+                _.each(upgrade.modifier, function (modifierFn, modifierName) {
+                    _.times(level, function() {
+                        $scope.playerShip[upgrade.system][modifierName] += modifierFn(level);
+                    });
+                });
+            });
+        }
 
         calculateShipStats();
 
-        function buyUpgrade(upgradeName)
-        {
+        function buyUpgrade(upgradeName) {
             var upgrade = upgrades[upgradeName];
             var nextUpgradeLevel = $scope.playerShip.upgrades[upgradeName] + 1;
             var nextUpgradeCost = upgrade.cost(nextUpgradeLevel);
