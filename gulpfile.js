d0b7c1dfaa211a3880695b1f466609133643ec2b
Joe Bandenburg <joe@bandenburg.com>
2014-11-09 10:10:27 +0000

    Add jscs style checker and enforce small set of rules

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
 
 gulp.task('scripts', function () {
     return gulp.src('src/**/*.js')
         .pipe(jshint('.jshintrc'))
         .pipe(jshint.reporter('default'))
+        .pipe(jscs())
 });
 
 gulp.task('default', function () {
